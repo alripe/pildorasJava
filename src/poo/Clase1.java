fb715e558612a4e9c2aa0954f1ef2d7e590a45c1
AlejandroRivera <alpe60@outlook.com>
2020-08-20 14:35:53 -0500

    Ejercicio Modificadores de Acceso JARP

---
+++ src/poo/Clase1.java
@@ -0,0 +1,11 @@
+package poo;
+
+public class Clase1 {
+
+    protected int miVar1=5;
+    protected int miVar2=10;
+
+    protected String miMetodo (){
+        return "El valor de mi variable es =: " + miVar2;
+    }
+}
---
+++ src/poo/Clase2.java
@@ -0,0 +1,13 @@
+package poo;
+import paquetepruebas.Clase3;
+
+public class Clase2 {
+    public static void main(String[] args){
+
+        Clase1 miObj = new Clase1();
+        Clase3 miObj1 = new Clase3();
+
+
+
+    }
+}
